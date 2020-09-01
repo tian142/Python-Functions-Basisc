985d5f7d052ffd0ff011af35d8e46d252c36e787
Tian Qiu <tian.qiu142@gmail.com>
2020-09-01 16:57:34 -0700

    first commit

---
+++ functions.py
@@ -0,0 +1,37 @@
+def area(width, height):
+    result = width * height
+    return result
+
+
+result = area(5, 6)
+print(result)
+
+result2 = area(9, 21)
+print(result2)
+
+result3 = area(21, 21)
+print(result3)
+
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+
+diff = subtract(420, 69)
+print(diff)
+diff2 = subtract(0, 99)
+print(diff2)
+
+
+def devide(num1, num2):
+    quotient = num1 / num2
+    return quotient
+
+
+def add(num1, num2):
+    sum = num1 + num2
+    return sum
+
+
+print(add(420, 69))
